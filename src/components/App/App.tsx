7f71b426bebab879a5aed9c48e7bc11334f99add
omeller-soldevelo <omeller@soldevelo.com>
2021-06-24 17:55:01 -0600

    [develop] Added routes & navigation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import { Main } from '../Main/Main';
+import { Navigation } from '../Navigation/Navigation';
+import { Page404 } from '../Page404/Page404';
 
-export const App: React.FC = () => <div className="App" />;
+export const App: React.FC = () => (
+  <Router>
+    <Navigation />
+    <Switch>
+      <Route exact={true} path="/">
+        <Redirect to="/main" />
+      </Route>
+      <Route path="/main">
+        <Main />
+      </Route>
+      <Route path="*">
+        <Page404 />
+      </Route>
+    </Switch>
+  </Router>
+);
