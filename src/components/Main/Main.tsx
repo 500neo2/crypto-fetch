be7d9bd423c44e3d9cf7f16711bcae7e86b655fd
omeller-soldevelo <omeller@soldevelo.com>
2021-06-24 19:09:09 -0600

    [develop] Added fetching data

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,3 +1,47 @@
-import React from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import axios from 'axios';
+import Select from 'react-select';
+import { ICoinData } from '../../types/Coin';
 
-export const Main: React.FC = () => <div>Main</div>;
+interface IOption {
+  value: string;
+  label: string;
+}
+
+const getOptions = (coins: ICoinData[]): IOption[] =>
+  coins.map((coin) => ({
+    value: coin.id,
+    label: coin.name,
+  }));
+
+export const Main: React.FC = () => {
+  const [fetchedCoins, setFetchedCoins] = useState<ICoinData[]>([]);
+  const [selectedCoin, setSelectedCoin] = useState(null);
+
+  useEffect((): void => {
+    axios
+      .get(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`,
+      )
+      .then((res) => {
+        const coins = res.data;
+        setFetchedCoins(coins);
+      });
+  }, []);
+
+  const handleChange = useCallback((val: any): void => {
+    setSelectedCoin(val);
+  }, []);
+
+  const options = useMemo(() => getOptions(fetchedCoins), [fetchedCoins]);
+
+  return (
+    <div>
+      <Select
+        defaultValue={selectedCoin}
+        onChange={handleChange}
+        options={options}
+      />
+    </div>
+  );
+};
---
+++ src/types/Coin.ts
@@ -0,0 +1,6 @@
+export interface ICoinData {
+  id: string;
+  current_price: number;
+  market_cap: number;
+  name: string;
+}
